a87f735e4f804c5288517ab49e9c9f5e666d727f
Petrus Silva <petrus.silva@toptal.com>
2026-05-24 19:29:10 -0700

    fix gke initial pool disk type to pd-standard to avoid ssd quota

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -32,6 +32,12 @@ resource "google_container_cluster" "main" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
+  node_config {
+    disk_type    = "pd-standard"
+    disk_size_gb = 30
+    machine_type = "e2-medium"
+  }
+
   network    = var.network_id
   subnetwork = var.subnetwork_id
 
